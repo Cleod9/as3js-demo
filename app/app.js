35ff70504edb6b2788f839fd84c9a6dc1adfcb92
Greg McLeod <cleod9@gmail.com>
2017-03-03 11:43:57 -0500

    -Bumped to AS3JS v0.3.2

--- app/app.js
+++ app/app.js
@@ -9732,7 +9732,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	/*******************************
-	  AS3JS Version 0.3.1
+	  AS3JS Version 0.3.2
 	  
 	    AS3 to JS converter for use with ImportJS and OOPS.js.
 	  
@@ -9791,7 +9791,7 @@
 	      // Some temps / helper
 	      var i, j, tmpPkg;
 	      var getPackageInfo = function ( name ) {
-	        // Splits package apth into separate package and class name
+	        // Splits package path into separate package and class name
 	        var pkg = name.split('.');
 	        var className = pkg[pkg.length-1];
 	        pkg.splice(pkg.length-1, 1);
@@ -9806,7 +9806,7 @@
 	      // This hash map contains each package, each package contains its classes
 	      var packages = {};
 
-	      // Converts supplied package hash to packageName.className.moduleFn
+	      // Converts supplied package hash to packageName.className.{ source: moduleFn }
 	      for (i in params.program) {
 	        tmpPkg = getPackageInfo(i);
 	        packages[tmpPkg.packageName] = packages[tmpPkg.packageName] || {};
@@ -9958,6 +9958,7 @@
 	      AS3Pattern.ASSIGN_UPTO = [new RegExp("[^;\\r\\n]", "g"), /(.*?)/g];
 	      AS3Pattern.VECTOR = [/new[\s\t]+Vector\.<(.*?)>\((.*?)\)/g, /new[\s\t]+Vector\.<(.*?)>\((.*?)\)/];
 	      AS3Pattern.ARRAY = [/new[\s\t]+Array\((.*?)\)/g, /new[\s\t]+Array\((.*?)\)/];
+	      AS3Pattern.DICTIONARY = [/new[\s\t]+Dictionary\((.*?)\)/g];
 	      AS3Pattern.REST_ARG = [/\.\.\.[a-zA-Z_$][0-9a-zA-Z_$]*/g, /\.\.\.[a-zA-Z_$][0-9a-zA-Z_$]*/g];
 	    };
 
@@ -9975,6 +9976,7 @@
 	    AS3Pattern.ASSIGN_UPTO = null;
 	    AS3Pattern.VECTOR = null;
 	    AS3Pattern.ARRAY = null;
+	    AS3Pattern.DICTIONARY = null;
 	    AS3Pattern.REST_ARG = null;
 
 	    module.exports = AS3Pattern;
@@ -10633,10 +10635,22 @@
 	        if (this.members[i] instanceof AS3Function) {
 	          allFuncs.push(this.members[i]);
 	        }
+	        if (this.members[i] instanceof AS3Variable) {
+	          // Fix any obvious assignments that rely on implicit static class name (only works for simple statements)
+	          if (this.members[i].value && this.retrieveField(this.members[i].value.replace(/^([a-zA-Z_$][0-9a-zA-Z_$]*)(.*?)$/g, "$1"), true)) {
+	            this.members[i].value = this.className + '.' + this.members[i].value;
+	          }
+	        }
 	      }
 	      for (i in this.staticMembers) {
 	        if (this.staticMembers[i] instanceof AS3Function) {
 	          allStaticFuncs.push(this.staticMembers[i]);
+	        }
+	        if (this.staticMembers[i] instanceof AS3Variable) {
+	          // Fix any obvious assignments that rely on implicit static class name (only works for simple statements)
+	          if (this.staticMembers[i].value && this.retrieveField(this.staticMembers[i].value.replace(/^([a-zA-Z_$][0-9a-zA-Z_$]*)(.*?)$/g, "$1"), true)) {
+	            this.staticMembers[i].value = this.className + '.' + this.staticMembers[i].value;
+	          }
 	        }
 	      }
 
@@ -11481,10 +11495,16 @@
 	          text = text.replace(AS3Pattern.ARRAY[1], "[]");
 	        }
 	      }
-	      //Take care of function binding
+
+	      matches = text.match(AS3Pattern.DICTIONARY[0]);
+	      //For each instantiated Dictionary found in the text
+	      for (i in matches) {
+	        // Replace with empty object
+	        text = text.replace(AS3Pattern.DICTIONARY[0], "{}");
+	      }
 
 	      //Now cleanup variable types
-	      text = text.replace(/([^0-9a-zA-Z_$.])var(\s*[a-zA-Z_$*][0-9a-zA-Z_$.<>]*)\s*:\s*([a-zA-Z_$*][0-9a-zA-Z_$.<>]*)/g, "$1var$2");
+	      text = text.replace(/([^0-9a-zA-Z_$.])(?:var|const)(\s*[a-zA-Z_$*][0-9a-zA-Z_$.<>]*)\s*:\s*([a-zA-Z_$*][0-9a-zA-Z_$.<>]*)/g, "$1var$2");
 
 	      return text;
 	    };
@@ -11572,10 +11592,7 @@
 	              currToken = AS3Parser.nextWord(src, index, AS3Pattern.IDENTIFIER[0], AS3Pattern.IDENTIFIER[1]);
 	              index = currToken.index;
 	              //The token following 'extends' must be the parent class
-	              /* NOTE: Commenting this out, used to be hard-coding to prevent flash packages from importing  */
-	              if ([ /*'MovieClip', 'Sprite', 'DisplayObject', 'DisplayObjectContainer'*/ ].indexOf(currToken.token) < 0) {
-	                cls.parent = currToken.token;
-	              }
+	              cls.parent = currToken.token;
 	              //Prep the next token
 	              currToken = AS3Parser.nextWord(src, index, AS3Pattern.IDENTIFIER[0], AS3Pattern.IDENTIFIER[1]);
 	              Main.debug("Found parent: " + cls.parent);
@@ -12200,14 +12217,103 @@
 /***/ function(module, exports) {
 
 	// shim for using process in browser
-
 	var process = module.exports = {};
+
+	// cached from whatever global is present so that test runners that stub it
+	// don't break things.  But we need to wrap it in a try catch in case it is
+	// wrapped in strict mode code which doesn't define any globals.  It's inside a
+	// function because try/catches deoptimize in certain engines.
+
+	var cachedSetTimeout;
+	var cachedClearTimeout;
+
+	function defaultSetTimout() {
+	    throw new Error('setTimeout has not been defined');
+	}
+	function defaultClearTimeout () {
+	    throw new Error('clearTimeout has not been defined');
+	}
+	(function () {
+	    try {
+	        if (typeof setTimeout === 'function') {
+	            cachedSetTimeout = setTimeout;
+	        } else {
+	            cachedSetTimeout = defaultSetTimout;
+	        }
+	    } catch (e) {
+	        cachedSetTimeout = defaultSetTimout;
+	    }
+	    try {
+	        if (typeof clearTimeout === 'function') {
+	            cachedClearTimeout = clearTimeout;
+	        } else {
+	            cachedClearTimeout = defaultClearTimeout;
+	        }
+	    } catch (e) {
+	        cachedClearTimeout = defaultClearTimeout;
+	    }
+	} ())
+	function runTimeout(fun) {
+	    if (cachedSetTimeout === setTimeout) {
+	        //normal enviroments in sane situations
+	        return setTimeout(fun, 0);
+	    }
+	    // if setTimeout wasn't available but was latter defined
+	    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+	        cachedSetTimeout = setTimeout;
+	        return setTimeout(fun, 0);
+	    }
+	    try {
+	        // when when somebody has screwed with setTimeout but no I.E. maddness
+	        return cachedSetTimeout(fun, 0);
+	    } catch(e){
+	        try {
+	            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+	            return cachedSetTimeout.call(null, fun, 0);
+	        } catch(e){
+	            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+	            return cachedSetTimeout.call(this, fun, 0);
+	        }
+	    }
+
+
+	}
+	function runClearTimeout(marker) {
+	    if (cachedClearTimeout === clearTimeout) {
+	        //normal enviroments in sane situations
+	        return clearTimeout(marker);
+	    }
+	    // if clearTimeout wasn't available but was latter defined
+	    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+	        cachedClearTimeout = clearTimeout;
+	        return clearTimeout(marker);
+	    }
+	    try {
+	        // when when somebody has screwed with setTimeout but no I.E. maddness
+	        return cachedClearTimeout(marker);
+	    } catch (e){
+	        try {
+	            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+	            return cachedClearTimeout.call(null, marker);
+	        } catch (e){
+	            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+	            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+	            return cachedClearTimeout.call(this, marker);
+	        }
+	    }
+
+
+
+	}
 	var queue = [];
 	var draining = false;
 	var currentQueue;
 	var queueIndex = -1;
 
 	function cleanUpNextTick() {
+	    if (!draining || !currentQueue) {
+	        return;
+	    }
 	    draining = false;
 	    if (currentQueue.length) {
 	        queue = currentQueue.concat(queue);
@@ -12223,7 +12329,7 @@
 	    if (draining) {
 	        return;
 	    }
-	    var timeout = setTimeout(cleanUpNextTick);
+	    var timeout = runTimeout(cleanUpNextTick);
 	    draining = true;
 
 	    var len = queue.length;
@@ -12240,7 +12346,7 @@
 	    }
 	    currentQueue = null;
 	    draining = false;
-	    clearTimeout(timeout);
+	    runClearTimeout(timeout);
 	}
 
 	process.nextTick = function (fun) {
@@ -12252,7 +12358,7 @@
 	    }
 	    queue.push(new Item(fun, args));
 	    if (queue.length === 1 && !draining) {
-	        setTimeout(drainQueue, 0);
+	        runTimeout(drainQueue);
 	    }
 	};
 
